480f63e8f92e6d8dcc99566007df7bbd7b2c8acf
ArgAbel <a7m7g7@gmail.com>
2025-08-16 22:38:57 -0300

    ejercicio 1 rehacer el js, 2 listo

---
+++ jscriptej2.js
@@ -0,0 +1,17 @@
+
+
+
+     
+        addEventListener('submit', function(event) {
+        event.preventDefault();
+        const num1 = document.getElementById('numero1').value;
+        const num2 = document.getElementById('numero2').value;
+        const mensaje1 = num1 - num2;
+        const mensaje2 = num1 + num2;
+        const mensaje3 = num1 / num2;
+        const mensaje4 = num1 * num2;
+        document.getElementById('mensajer').textContent = mensaje1;
+         document.getElementById('mensajes').textContent = mensaje2;
+          document.getElementById('mensajed').textContent = mensaje3;
+           document.getElementById('mensajem').textContent = mensaje4;
+        })
